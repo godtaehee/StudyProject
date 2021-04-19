f41d8829ee93be8ba8797007dbf7b7bc536446eb
rhacnddl <jiho519@naver.com>
2021-04-19 17:19:35 +0900

    mypage complete

--- js/mypage/friends.js
+++ js/mypage/friends.js
@@ -1,4 +1,15 @@
 window.addEventListener("load", function(){
 
-
+	var list = document.querySelector('.content-list');
+	
+	list.addEventListener("click", function(e) {
+		
+		var targetName = e.target.tagName;
+		
+		if(targetName === 'BUTTON'){
+			if(confirm('친구를 삭제하시겠습니까?')){
+				
+			}
+		}	
+	});
 })
